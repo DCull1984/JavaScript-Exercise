85e23ad49ac33f4e43bbdb03e9cfd80a0cbe003d
Your Name <you@example.com>
2019-01-16 16:56:46 +0000

    end of day

--- JS_Exercise_Level_2/JS_Exercise_2.02/js_main.js
+++ JS_Exercise_Level_2/JS_Exercise_2.02/js_main.js
@@ -1,16 +1,16 @@
 function reduceToOne(){
 	let n = +document.getElementById("Num").value;
+
 	do{
 		if(n%3 === 0){
 			n = n/3;
 		}
-		else if(n%3 != 0){
-			if(n%3 != 0){
-				n+=1;
-			}
-			else{
-			n-=1;}
-			}
+		//else if((n+1)%3 == 0){
+		//	n = n/3;
+		//}
+		else if((n-1)%3 == 0){
+			n = n/3;
+		}
 		else{
 		
 		}
